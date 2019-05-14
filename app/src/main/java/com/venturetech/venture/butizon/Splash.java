cd5862eb21ad77b72f3f43b0e7f3d82bcac5e919
Tomson <tomsonthomas68@gmail.com>
2019-05-14 17:43:30 +0530

    second commit

--- app/src/main/java/com/venturetech/venture/butizon/Splash.java
+++ app/src/main/java/com/venturetech/venture/butizon/Splash.java
@@ -1,14 +1,15 @@
 package com.venturetech.venture.butizon;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-import com.venturetech.venture.butizon.ClubApp.ClubLogin;
 import com.venturetech.venture.butizon.ClubApp.ClubMain;
-import com.venturetech.venture.butizon.StartActivity.Slider;
 import com.venturetech.venture.butizon.StartActivity.Sliding;
 import com.venturetech.venture.butizon.UserLogin.UserActivity;
 import com.venturetech.venture.butizon.databases.DBTransactionFunctions;
@@ -23,46 +24,62 @@ DBTransactionFunctions dbTransactionFunctions;
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_OVERSCAN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_OVERSCAN);
         setContentView(R.layout.activity_splash);
         dbTransactionFunctions =new DBTransactionFunctions(getApplicationContext());
-        if(DBTransactionFunctions.getConfigvalue("inserterd").equals("1")){
-
-        }else {
-            DBTransactionFunctions.insertConfigValues();
-            DBTransactionFunctions.updateConfigvalues("inserterd","1");
-        }
-
-        if(DBTransactionFunctions.getConfigvalue("login_status").equals("1")){
-            if(DBTransactionFunctions.getConfigvalue("user_type").equals("1")){
-                Intent intent=new Intent(Splash.this,ClubMain.class);
-                startActivity(intent);
-                finish();
-            }else {
-                Intent intent1 = new Intent(Splash.this, UserActivity.class);
-                startActivity(intent1);
-                finish();
-            }
-        }else{
-            try {
-                Thread timer = new Thread() {
-                    public void run() {
-                        try {
-                            sleep(3000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } finally {
 
 
-                            Intent i = new Intent(Splash.this,Sliding.class);
-                            startActivity(i);
 
-                        }
-                    }
-                };
-                timer.start();
+                                          if(isNetworkAvailable(Splash.this))
+                                          {
+                                              if(DBTransactionFunctions.getConfigvalue("inserterd").equals("1")){
 
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
+                                              }else {
+                                                  DBTransactionFunctions.insertConfigValues();
+                                                  DBTransactionFunctions.updateConfigvalues("inserterd","1");
+                                              }
+
+                                              if(DBTransactionFunctions.getConfigvalue("login_status").equals("1")){
+                                                  if(DBTransactionFunctions.getConfigvalue("user_type").equals("1")){
+                                                      Intent intent=new Intent(Splash.this,ClubMain.class);
+                                                      startActivity(intent);
+                                                      finish();
+                                                  }else {
+                                                      Intent intent1 = new Intent(Splash.this, UserActivity.class);
+                                                      startActivity(intent1);
+                                                      finish();
+                                                  }
+                                              }else{
+                                                  try {
+                                                      Thread timer = new Thread() {
+                                                          public void run() {
+                                                              try {
+                                                                  sleep(3000);
+                                                              } catch (InterruptedException e) {
+                                                                  e.printStackTrace();
+                                                              } finally {
+                                                                  if(isNetworkAvailable(Splash.this)) {
+                                                                      Intent i = new Intent(Splash.this, Sliding.class);
+                                                                      startActivity(i);
+                                                                  }else {
+                                                                      Toast.makeText(Splash.this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+                                                                  }
+
+                                                              }
+                                                          }
+                                                      };
+                                                      timer.start();
+
+                                                  }catch (Exception e){
+                                                      e.printStackTrace();
+                                                  }
+                                              }
+                                          }else {
+                                              Toast.makeText(Splash.this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+
+
+                                          }
+
+
+
+
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -75,6 +92,11 @@ DBTransactionFunctions dbTransactionFunctions;
         // TODO Auto-generated method stub
         super.onPause();
         finish();
+    }
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
     }
 
 }
